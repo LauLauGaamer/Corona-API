c5f2c2c66219fece128af2014a1a5b2c7d28671e
Lauryn <lauryndube1@gmail.com>
2024-07-31 11:53:25 +0200

    add DataBase

--- app/corona/models.py
+++ app/corona/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class States(models.Model):
+        id = models.IntegerField(primary_key=True)
+        name = models.CharField(max_length=200)
+        abbreviation = models.CharField(max_length=5)
+
+class County(models.Model):
+        id = models.IntegerField(primary_key=True)
+        name = models.CharField(max_length=200)
+        state = models.ForeignKey(States, on_delete=models.PROTECT)
+
+class Districts(models.Model):
+        id = models.IntegerField(primary_key=True)
+        state = models.ForeignKey(States, on_delete=models.PROTECT)
+        county = models.ForeignKey(County, on_delete=models.PROTECT)
+        name = models.CharField(max_length=200)
